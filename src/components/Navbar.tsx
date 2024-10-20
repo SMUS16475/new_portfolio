8b80f5cb305ef300cff07daafeae00e0d834c368
SMUS16475 <echiobi2000@gmail.com>
2024-10-20 19:48:40 -0400

    Continuing work on portfolio

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <ul>
+                <li><a href=''></a>Home</li>
+                <li><a></a>Projects</li>
+                <li><a></a>Contact</li>
+            </ul>
+        </>
+    )
+}
+
+export default Navbar
