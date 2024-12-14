071298cc9c95e1d8a3ec39edfb390a88d4cf1e41
SMUS16475 <echiobi2000@gmail.com>
2024-12-14 04:13:13 -0500

    ...Why am I still awake?

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <>
-            <ul>
-                <li><a href=''></a>Home</li>
-                <li><a></a>Projects</li>
-                <li><a></a>Contact</li>
-            </ul>
-        </>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
     )
 }
 
