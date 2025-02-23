50797195641b231c4ba1d47d4cbe537dc7224f35
SMUS16475 <echiobi2000@gmail.com>
2025-02-22 19:41:49 -0500

    Adding back button to 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const PageNotFound = () => {
+  const navi = useNavigate();
   return (
     <>
+      <p onClick={() => navi(-1)}><a href="#">Back</a></p>
       <h1>404 Not Found</h1>
       <p>The page you are looking for does not exist.</p>
     </>
